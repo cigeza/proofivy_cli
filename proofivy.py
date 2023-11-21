0d40c7a6d65b979bc20d63dd7520ec06cab669c6
Cigeza <contact@cigeza.com>
2023-11-21 13:59:23 +0100

    default sha256 hash for CID now with optional argument for blake3

--- proofivy.py
+++ proofivy.py
@@ -1,17 +1,29 @@
 # Includes code from:
 # https://github.com/thunderstore-io/ipfs-cid
 
+from hashlib import sha256
 from blake3 import blake3
 from base64 import b32encode
 import argparse
 
 MULTICODEC_CIDV1 = b"\x01"
 MULTICODEC_RAW_BINARY = b"\x55"
-MULTICODEC_LENGTH_256 = b"\x20"
+MULTICODEC_SHA_2_256 = b"\x12"
 MULTICODEC_BLAKE3 = b"\x1e"
+MULTICODEC_LENGTH_256 = b"\x20"
 PREFIX = "b"
 
-CID_PREFIX = b"".join(
+
+CID_PREFIX_SHA256 = b"".join(
+    [
+        MULTICODEC_CIDV1,
+        MULTICODEC_RAW_BINARY,
+        MULTICODEC_SHA_2_256,
+        MULTICODEC_LENGTH_256,
+    ],
+)
+
+CID_PREFIX_BLAKE3 = b"".join(
     [
         MULTICODEC_CIDV1,
         MULTICODEC_RAW_BINARY,
@@ -26,22 +38,35 @@ def encode(data: bytes) -> str:
     return PREFIX + b32.lower().replace("=", "")
 
 
-def cid_wrap_digest(digest: bytes) -> str:
+def cid_wrap_digest_sha256(digest: bytes) -> str:
     digest_len = len(digest).to_bytes(1, "big")
     if digest_len != MULTICODEC_LENGTH_256:
         raise AttributeError("Invalid digest length")
-    return encode(CID_PREFIX + digest)
+    return encode(CID_PREFIX_SHA256 + digest)
+
+
+def cid_wrap_digest_blake3(digest: bytes) -> str:
+    digest_len = len(digest).to_bytes(1, "big")
+    if digest_len != MULTICODEC_LENGTH_256:
+        raise AttributeError("Invalid digest length")
+    return encode(CID_PREFIX_BLAKE3 + digest)
 
 
 def main():
     parser = argparse.ArgumentParser(epilog="PROOFIVY - https://proofivy.com")
-    parser.add_argument("filename", help="file directory for BLAKE3 hash calculation")
+    parser.add_argument("filename", help="file directory for hash calculation")
+    parser.add_argument("--blake3", action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
     with open(args.filename, mode='rb') as file:
         file_to_hash = file.read()
-    blake3_file = blake3(file_to_hash).digest()
-    hash_string = cid_wrap_digest(blake3_file)
-    print(f"{hash_string}")
+    if args.blake3:
+        blake3_file = blake3(file_to_hash).digest()
+        hash_string = cid_wrap_digest_blake3(blake3_file)
+        print(f"{hash_string}")
+    else:
+        sha256_file = sha256(file_to_hash).digest()
+        hash_string = cid_wrap_digest_sha256(sha256_file)
+        print(f"{hash_string}")
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name="proofivy-cli-app",
-    version="0.1.0",
-    description="Proofivy app to calculate BLAKE3 hashes of files",
+    version="0.2.0",
+    description="Proofivy app to calculate hashes of files",
     py_modules=["proofivy"],
     entry_points={
         "console_scripts": [
